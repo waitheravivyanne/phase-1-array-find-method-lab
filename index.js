c652d42d15ae05de7f858180f88f6f0cded51a5e
vivian waithera <vivyannekori@gmail.com>
2023-06-20 15:41:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin (collection){
+for (const item of collection) {
+    if (item.result==="W") {
+        return item.year
+        {
+            return undefined
+        }
+    }
+    }
+}
+record.find(superbowlWin)
